49dad0ab9427b652261c33a8f7d7f836af05fa52
Aya Maher <aya.nassan2000@gmail.com>
2021-06-05 23:42:45 +0300

    commit N

--- app/src/main/java/com/example/cloudass_3/Sign1.java
+++ app/src/main/java/com/example/cloudass_3/Sign1.java
@@ -1,4 +1,112 @@
 package com.example.cloudass_3;
 
-public class Sign1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+
+public class Sign1 extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    EditText firstName,secondName,email,password;
+    Button signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        firstName=(EditText)findViewById(R.id.firstName);
+        secondName=(EditText)findViewById(R.id.secondName);
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+        signup=(Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data="{"+
+                        "\"firstName\""   + ":"+  "\""+firstName.getText().toString()+"\","+
+                        "\"secondName\""  + ":"+  "\""+secondName.getText().toString()+"\","+
+                        "\"email\""       + ":"+  "\""+email.getText().toString()+"\","+
+                        "\"password\""    + ":"+   "\""+password.getText().toString()+"\""+
+                        "}";
+                Submit(data);
+                getRegToken();
+                Intent i = new Intent(Sign1.this, Sign.class);
+                startActivity(i);
+            }
+        });
+    }
+    private void getRegToken() {
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(!task.isSuccessful()){
+                    Log.w("TAG","Falid to get token"+task.getException());
+                    return;
+                }
+                String token = task.getResult();
+                Log.d("TAG","Token "+ token);
+
+            }
+        });
+    }
+    private void Submit(String data) {
+        String savedata=data;
+        String URL="https://mcc-users-api.herokuapp.com/add_new_user";
+
+        requestQueue= Volley.newRequestQueue(getApplicationContext());
+        Log.d("TAG", "requestQueue: "+requestQueue);
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject objres = new JSONObject(response);
+                    Log.d("TAG", "onResponse: "+objres.toString());
+                } catch (JSONException e) {
+                    Log.d("TAG", "Server Error ");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG", "onErrorResponse: "+error);
+            }
+        })
+        {
+            @Override
+            public String getBodyContentType(){return "application/json; charset=utf-8";}
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try{
+                    Log.d("TAG", "savedata: "+savedata);
+                    return savedata==null?null:savedata.getBytes("utf-8");
+                }catch(UnsupportedEncodingException uee){
+                    return null;
+                }
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
 }
